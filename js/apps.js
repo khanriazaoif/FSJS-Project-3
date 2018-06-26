059c8e5dab83377d3d18003e743f0ff4b9cc7f14
Riaz Khan <khanriazaoif@gmail.com>
2018-06-25 20:19:36 -0500

    Onchange event on the Design dropdown
    I have created functionality that when you click on the dropdown under the Design section, it will console.log “That’s correct” if the value equals “js puns”

--- js/apps.js
+++ js/apps.js
@@ -3,3 +3,19 @@
 window.onload = function() {
     document.getElementById("name").focus();
   };
+
+  //”T-Shirt Info” section of the form:
+//For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
+function tshirtColor(){
+design.onchange = function(){
+  let design=document.getElementById("design");
+  if(design[design.selectedIndex].value == "js puns") {
+    console.log("That's correct!"); 
+  } else {
+    console.log("You're wrong");
+  }
+ }
+}
+
+
+tshirtColor();
