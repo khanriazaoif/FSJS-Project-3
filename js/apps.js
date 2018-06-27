a557806e58d67d9c86f3f572575bba94ff4d771c
Riaz Khan <khanriazaoif@gmail.com>
2018-06-26 20:30:51 -0500

    Added ability to search Design field
    I am now able to select design JS Puns and it then looks at includes and checks for anything with JS Puns in it then console.log “I love it”
    
    This tells me I am able to select the two drop downs and also use includes on the string to find what has JS Puns or not.

--- js/apps.js
+++ js/apps.js
@@ -6,18 +6,23 @@ window.onload = function() {
 
   //”T-Shirt Info” section of the form:
 //For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
+
 function tshirtColor(){
 design.onchange = function(){
-  let design=document.getElementById("design");
+  let design = document.getElementById("design");
   if(design[design.selectedIndex].value == "js puns") {
-    let jsPuns = document.getElementById("design").value;
-    console.log(jsPuns);
+    let colorId = document.getElementById("color");
+    for(let i = 0; i < colorId.length; i++ ){
+      let a = colorId.options[i].text;
+      if(colorId[i].text.includes("JS Puns")){
+        console.log("I love it");
+      }
+    }
   } else {
     let heartJs = document.getElementById("design").value;
     console.log(heartJs);
   }
  }
 }
-
 
 tshirtColor();
