26a8f1917d7b0e209ac29db9a649351170996603
Riaz Khan <khanriazaoif@gmail.com>
2018-08-15 13:13:58 -0500

    Updated the design theme picker to filter for all different searches

--- js/apps.js
+++ js/apps.js
@@ -11,10 +11,10 @@ window.onload = function() {
 
 function tshirtColor(){
 design.onchange = function(){
-  if(design[design.selectedIndex].value == "js puns") {
+  console.log(design[design.selectedIndex].value);
+  if(design[design.selectedIndex].value === "js puns" && "heart js" && "Select Theme"){
     let colorId = document.getElementById("color");
     for(let i = 0; i < colorId.length; i++ ){
-      let a = colorId.options[i].text;
       if(colorId[i].text.includes("JS Puns")){
         colorId[i].style.display = "block";
       } else {
@@ -22,19 +22,32 @@ design.onchange = function(){
       }
         
       }
-    } else {
+    } else if (design[design.selectedIndex].value === "heart js"){
+      console.log(design[design.selectedIndex].value);
       let colorId = document.getElementById("color");
       for(let i = 0; i < colorId.length; i++ ){
-        let a = colorId.options[i].text;
         if(colorId[i].text.includes("JS shirt only")){
+          console.log(colorId[i]);
           colorId[i].style.display = "block";
         } else {
           colorId[i].style.display = "none";
           }
           
         }
+    } else {
+      console.log(design[design.selectedIndex].value);
+      let colorId = document.getElementById("color");
+      for(let i = 0; i < colorId.length; i++ ){
+        if(colorId[i].text.includes("")){
+          colorId[i].style.display = "block";
+        } else {
+          colorId[i].style.display = "none";
+          }
+          
+        }
+      
+    }
   }
- }
 }
 
 tshirtColor();
