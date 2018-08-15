4112e8cd22698852d3d001bbf3edd19b108ff8aa
Riaz Khan <khanriazaoif@gmail.com>
2018-08-15 14:19:58 -0500

    Latest update

--- js/apps.js
+++ js/apps.js
@@ -1,59 +1,58 @@
 //Set focus on the first text field
 //When the page loads, give focus to the first text field
 window.onload = function() {
-    document.getElementById("name").focus();
-  };
+  document.getElementById("name").focus();
+};
 
-  let design = document.getElementById("design");
+let design = document.getElementById("design");
 
-  //”T-Shirt Info” section of the form:
+//”T-Shirt Info” section of the form:
 //For the T-Shirt "Color" menu, only display the color options that match the design selected in the "Design" menu.
 
-function hideAndShowJobRoleOnOther(){
-   const valueOther = document.getElementById('other-title');
-   valueOther.style.display = "none";
-   const selectId = document.getElementById('title');   
-   selectId.onchange = function showJobRoleOnOther(){
-       valueOther.style.display = "block";
-      }
-  }
+function hideAndShowJobRoleOnOther() {
+  const valueOther = document.getElementById("other-title");
+  valueOther.style.display = "none";
+  const selectId = document.getElementById("title");
+  selectId.onchange = function showJobRoleOnOther() {
+    valueOther.style.display = "block";
+  };
+}
 
-
-function tshirtColor(){
-design.onchange = function(){
-  if(design[design.selectedIndex].value === "js puns" && "heart js" && "Select Theme"){
-    let colorId = document.getElementById("color");
-    for(let i = 0; i < colorId.length; i++ ){
-      if(colorId[i].text.includes("JS Puns")){
-        colorId[i].style.display = "block";
-      } else {
-        colorId[i].style.display = "none";
-      }
-        
-      }
-    } else if (design[design.selectedIndex].value === "heart js"){
+function tshirtColor() {
+  design.onchange = function() {
+    if (
+      design[design.selectedIndex].value === "js puns" &&
+      "heart js" &&
+      "Select Theme"
+    ) {
       let colorId = document.getElementById("color");
-      for(let i = 0; i < colorId.length; i++ ){
-        if(colorId[i].text.includes("JS shirt only")){
+      for (let i = 0; i < colorId.length; i++) {
+        if (colorId[i].text.includes("JS Puns")) {
           colorId[i].style.display = "block";
         } else {
           colorId[i].style.display = "none";
-          }
-          
         }
+      }
+    } else if (design[design.selectedIndex].value === "heart js") {
+      let colorId = document.getElementById("color");
+      for (let i = 0; i < colorId.length; i++) {
+        if (colorId[i].text.includes("JS shirt only")) {
+          colorId[i].style.display = "block";
+        } else {
+          colorId[i].style.display = "none";
+        }
+      }
     } else {
       let colorId = document.getElementById("color");
-      for(let i = 0; i < colorId.length; i++ ){
-        if(colorId[i].text.includes("")){
+      for (let i = 0; i < colorId.length; i++) {
+        if (colorId[i].text.includes("")) {
           colorId[i].style.display = "block";
         } else {
           colorId[i].style.display = "none";
-          }
-          
         }
-      
+      }
     }
-  }
+  };
 }
 hideAndShowJobRoleOnOther();
 tshirtColor();
